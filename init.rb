8775ecfeb549044a57dc275d055d6c139d224345
Grigoriy Vasilchenko <grigoriy.vasilchenko@gmail.com>
2018-03-16 12:48:42 +0300

    Added info to README.rdoc

--- init.rb
+++ init.rb
@@ -1,9 +1,9 @@
 Redmine::Plugin.register :redmine_scm_path_view do
   name 'Scm Path View plugin'
   author 'Grigoriy Vasilchenko'
-  description 'This is a plugin for Redmine. It modifies "Repository" page and adds repository path (url in common)'
+  description 'Plugin adds repository path (url in common) "Repository" page'
   version '0.0.1'
-  url ''
+  url 'https://github.com/11v1/redmine_scm_path_view'
   author_url 'mailto:grigoriy.vasilchenko@gmail.com'
   
   require_dependency 'hooks/view_repositories'
