1bbad0dc0bc7b8cdf78edec343bfb25923901342
Grigoriy Vasilchenko <grigoriy.vasilchenko@gmail.com>
2018-03-16 10:49:40 +0300

    First version. Adds just SCM type and path (url) to "Repository" page

---
+++ init.rb
@@ -0,0 +1,10 @@
+Redmine::Plugin.register :scm_path_view do
+  name 'Scm Path View plugin'
+  author 'Grigoriy Vasilchenko'
+  description 'This is a plugin for Redmine. It modifies "Repository" page and adds repository path (url in common)'
+  version '0.0.1'
+  url ''
+  author_url 'mailto:grigoriy.vasilchenko@gmail.com'
+  
+  require_dependency 'hooks/view_repositories'
+end
---
+++ lib/hooks/view_repositories.rb
@@ -0,0 +1,21 @@
+
+
+module SCMPathView
+
+	class ViewRepositoriesHook < Redmine::Hook::ViewListener
+		
+		render_on :view_repositories_show_contextual, :partial => 'scm_path_view/repositories_show'
+		
+=begin
+		def view_repositories_show_contextual(context = {})
+
+			@repository = context[:repository]
+			context[:controller].send(:render_to_string, { :partial => 'scm_path_view/show', :locals => context })
+			#return self.class
+		
+		end
+=end
+
+	end
+
+end
